d1fe42af36426921fbcfb1e57843b0875871bad6
ra-shree <96831449+ra-shree@users.noreply.github.com>
2022-01-26 08:28:12 +0545

    changing some variables to constants

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let score = 0;
 
 // generates a random number using Math and uses it to decide computer's move for that round
 function computerChoice() {
-    let randomNumber = Math.floor(Math.random() * 10) + 1;
-    let computerMove;
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    let computerMove = "";
     if (randomNumber === 1 || randomNumber === 5 || randomNumber === 7) {
         computerMove = "Rock";
     } else if (randomNumber === 3 || randomNumber === 6 || randomNumber === 9) {
@@ -16,46 +16,41 @@ function computerChoice() {
 
 // function to check whether the player or computer won the last round
 // returns win if player won and returns loss if computer won and returns draw for draw
-let winOrLose = (playerChoice, computerChoice) => {
+const winOrLose = (playerChoice, computerChoice) => {
     switch (playerChoice) {
         case "Rock":
             if (computerChoice === "Paper") {
                 return ("loss");
             } else if (computerChoice === "Scissor") {
                 return ("win");
-            } else {
-                return ("draw");
             }
         case "Paper":
             if (computerChoice === "Scissor") {
                 return ("loss");
             } else if (computerChoice === "Rock") {
                 return ("win");
-            } else {
-                return ("draw");
             }
         case "Scissor":
             if (computerChoice === "Paper") {
                 return ("loss");
             } else if (computerChoice === "Rock") {
                 return ("win");
-            } else {
-                return ("draw");
             }
+            return ("draw");
     }
 }
 
 // displaying the latest move made by the player and computer
 function currentMove(playerMove, computerMove) {
-    let latestplayerMove = document.getElementById("latest-player-move");
-    let latestcomputerMove = document.getElementById("latest-computer-move");
+    const latestplayerMove = document.getElementById("latest-player-move");
+    const latestcomputerMove = document.getElementById("latest-computer-move");
     latestplayerMove.innerText = playerMove;
     latestcomputerMove.innerText = computerMove;
 }
 
 // decides if the round was win or loss and then updates the global variable score and also the DOM score
 function updateScore(result) {
-    let displayedScore = document.querySelector(".score");
+    const displayedScore = document.querySelector(".score");
     if (result === "win") {
         score++
     } else if (result === "loss") {
@@ -78,8 +73,8 @@ function winner() {
 
 // resets the game when the reset button is pressed
 function resetGame() {
-    let reset = document.querySelector("#reset-round");
-    reset.addEventListener("click", e => {
+    const reset = document.querySelector("#reset-round");
+    reset.addEventListener("click", () => {
         let element = document.getElementById("latest-player-move");
         element.innerText = "";
         element = document.getElementById("latest-computer-move");
@@ -101,12 +96,12 @@ function reset() {
 
 // checks which button was clicked and returns the move's name as a value
 function playerChoice() {
-    let moveSelection = document.querySelector(".move-selection");
+    const moveSelection = document.querySelector(".move-selection");
     moveSelection.addEventListener("click", e => {
-        let playerMove = e.path[0].innerText; // player move
-        let computerMove = computerChoice(); // computer move
+        const playerMove = e.path[0].innerText; // player move
+        const computerMove = computerChoice(); // computer move
         currentMove(playerMove, computerMove);
-        let result = winOrLose(playerMove, computerMove); // result of the two moves
+        const result = winOrLose(playerMove, computerMove); // result of the two moves
         updateScore(result); // updates the result of the moves in the HTML
         winner();
         resetGame();
